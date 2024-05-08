d3d4a19b646ee527b9afa732a6090b0bcefdbf3e
心中没有白月光 <1277215827@qq.com>
2024-05-08 17:10:56 +0800

    用户管理接口

--- blog-server/routes/users.js
+++ blog-server/routes/users.js
@@ -44,7 +44,7 @@ router.post("/", function (req, res, next) {
   const { username, password, email } = req.body.params;
 
   const sql = `INSERT INTO user
-  (userName, password,email)
+  (username, password,email)
   VALUES (?, ?, ?);`;
   db.query(sql, [username, password, email], (err, result) => {
     if (err) {
@@ -64,12 +64,49 @@ router.delete("/", function (req, res, next) {
 
 router.put("/", function (req, res, next) {
   //TODO: 更新用户信息
+  const { id, ...fields } = req.body;
+
+  // 创建 SQL 查询的 SET 部分
+  const setParts = [];
+  const values = [];
+  for (const [key, value] of Object.entries(fields)) {
+    if (key === "createTime") {
+      setParts.push(`${key} =FROM_UNIXTIME(?)`);
+      const date = new Date(value);
+      values.push(Math.floor(date.getTime() / 1000));
+    } else {
+      setParts.push(`${key} = ?`);
+      values.push(value);
+    }
+  }
+
+  // 如果没有接收到任何字段，返回错误
+  if (setParts.length === 0) {
+    return res.status(400).send("No fields to update");
+  }
+
+  // 创建 SQL 查询
+  const sql = `UPDATE user
+  SET ${setParts.join(", ")}
+  WHERE id = ?`;
+
+  // 执行 SQL 查询
+  db.query(sql, [...values, id], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Server error");
+    } else if (result.affectedRows === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(200).send("User updated");
+    }
+  });
 });
 
 router.get("/usersList", function (req, res, next) {
   db.query(
     `SELECT 
-    id,userName,role,avatar,nickname,commentLimit,email,createTime
+    id,username,role,avatar,nickname,commentLimit,email,createTime
     FROM user`,
     (err, data, _field) => {
       if (err) {
