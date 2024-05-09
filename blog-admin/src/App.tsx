81c904b3aa09ab5767f152322db55ca7896dc39d
心中没有白月光 <1277215827@qq.com>
2024-05-09 16:07:16 +0800

    提取头部组件

--- blog-admin/src/App.tsx
+++ blog-admin/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState, Suspense } from "react";
-import "./App.scss";
-import routerList from "@/utils/router";
-import { Breadcrumb, Layout, Menu, Popover, theme } from "antd";
+import { Suspense } from "react"
+import "./App.scss"
+import routerList from "@/utils/router"
+import { Layout, Menu } from "antd"
 
-const { Header, Content, Sider } = Layout;
+const { Content, Sider } = Layout
 
-import Loading from "@/pages/Loading";
-import { LoginOutlined, UserOutlined } from "@ant-design/icons";
+import Loading from "@/pages/Loading"
 
-import useUserStore, { logoutInfo } from "@/store/user";
+import AdminHeader from "./components/Header"
 
 function App() {
-  const { id, username, nickname, avatar } = useUserStore();
-  const navigate = useNavigate();
-  const router = useLocation();
+  const navigate = useNavigate()
   const menuItems = routerList.map((item) => {
-    if (item.meta?.showOnMenu === false) return null;
+    if (item.meta?.showOnMenu === false) return null
     return {
       key: item.path,
       icon: item.icon,
       label: item.name,
-    };
-  });
-  const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
-  const breadcrumbItem = [
-    { title: routerList.find((item) => item.path === router.pathname)?.name },
-  ];
+      children:item?.children?.map((child) => {
+        if (child.meta?.showOnMenu === false) return null
+        return {
+          key: child.path,
+          icon: child.icon,
+          label: child.name,
+        }
+      })
+    }
+  })
+  const [collapsed, setCollapsed] = useState(false)
 
   return (
     <>
@@ -48,45 +46,12 @@ function App() {
             mode="inline"
             items={menuItems}
             onClick={(item) => {
-              navigate(item.key);
+              navigate(item.key)
             }}
           />
         </Sider>
         <Layout>
-          <Header
-            className="header"
-            style={{
-              background: colorBgContainer,
-              padding: "5px",
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            <Breadcrumb
-              style={{
-                margin: "5px 10px",
-              }}
-              items={breadcrumbItem}
-            />
-            {id ? (
-              <div>
-                <Popover
-                  placement="bottomRight"
-                  content={<div onClick={logoutInfo}>退出</div>}
-                >
-                  <Avatar
-                    src={avatar}
-                    icon={avatar ? null : <UserOutlined />}
-                  />
-                  <span>{nickname || username}</span>
-                </Popover>
-              </div>
-            ) : (
-              <div onClick={()=>{navigate('/login')}}>
-                <LoginOutlined />
-                登录
-              </div>
-            )}
-          </Header>
+          <AdminHeader />
           <Content
             style={{
               margin: "5px 16px",
@@ -107,7 +72,7 @@ function App() {
         </Layout>
       </Layout>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ blog-admin/src/components/Header/index.tsx
@@ -0,0 +1,56 @@
+import useUserStore from "@/store/user"
+import "./index.scss"
+import { LoginOutlined, UserOutlined } from "@ant-design/icons"
+import { Breadcrumb, Layout, Popover, theme } from "antd"
+const { Header } = Layout
+
+export default function AdminHeader() {
+  const { id, username, nickname, avatar } = useUserStore()
+  const navigate = useNavigate()
+  const router = useLocation()
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
+
+  const breadcrumbItem = [
+    { title: routerList.find((item) => item.path === router.pathname)?.name },
+  ]
+
+  return (
+    <Header
+      className="header"
+      style={{
+        background: colorBgContainer,
+        padding: "5px",
+        borderRadius: borderRadiusLG,
+      }}
+    >
+      <Breadcrumb
+        style={{
+          margin: "5px 10px",
+        }}
+        items={breadcrumbItem}
+      />
+      {id ? (
+        <div>
+          <Popover
+            placement="bottomRight"
+            content={<div onClick={logoutInfo}>退出</div>}
+          >
+            <Avatar src={avatar} icon={avatar ? null : <UserOutlined />} />
+            <span>{nickname || username}</span>
+          </Popover>
+        </div>
+      ) : (
+        <div
+          onClick={() => {
+            navigate("/login")
+          }}
+        >
+          <LoginOutlined />
+          登录
+        </div>
+      )}
+    </Header>
+  )
+}
