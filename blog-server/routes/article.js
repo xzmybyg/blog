fe30a2b7fd8892297f99379ed636b63d0dd5a560
心中没有白月光 <1277215827@qq.com>
2024-05-09 14:02:25 +0800

    添加获取文章列表接口

--- blog-server/routes/article.js
+++ blog-server/routes/article.js
@@ -1,12 +1,14 @@
-var express = require("express");
-var router = express.Router();
-const db = require("../utils/mysqlUtils");
-const articleDataProcessing = require("../utils/articleDataProcessing");
-const checkToken = require("../middleware/checkToken");
+var express = require("express")
+var router = express.Router()
+const db = require("../utils/mysqlUtils")
+const articleDataProcessing = require("../utils/articleDataProcessing")
+const checkToken = require("../middleware/checkToken")
+const fs = require("fs")
+const path = require('path');
 
 //获取文章列表
 router.get("/", function (req, res, _next) {
-  const { page, pageSize, allList } = req.query;
+  const { page, pageSize, allList } = req.query
 
   let sql = allList
     ? `SELECT * FROM article 
@@ -14,18 +16,18 @@ router.get("/", function (req, res, _next) {
     : `SELECT * FROM article 
     WHERE hidden = 0
     ORDER BY topping DESC 
-    LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize}`;
+    LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize}`
 
   db.query(sql, (err, data, _field) => {
     if (err) {
-      console.error(err);
-      res.status(500).send("Server error");
+      console.error(err)
+      res.status(500).send("Server error")
     } else {
-      data = articleDataProcessing(data);
-      res.send(data);
+      data = articleDataProcessing(data)
+      res.send(data)
     }
-  });
-});
+  })
+})
 
 //新增文章
 router.post("/", checkToken, function (req, res, _next) {
@@ -37,95 +39,123 @@ router.post("/", checkToken, function (req, res, _next) {
     label, // 标签
     topping = 0, // 是否置顶，默认为0
     createTime = new Date(), // 创建日期，默认为当前日期
-  } = req.body;
+  } = req.body
 
   const sql = `INSERT INTO article 
   (title, description, article, label, topping, createTime) 
-  VALUES (?, ?, ?, ?, ?, ?)`;
+  VALUES (?, ?, ?, ?, ?, ?)`
   db.query(
     sql,
     [title, description, article, label, topping, createTime],
     (err, result) => {
       if (err) {
-        console.error(err);
-        res.status(500).send("Server error");
+        console.error(err)
+        res.status(500).send("Server error")
       } else {
-        res.status(201).send("Article created");
+        res.status(201).send("Article created")
       }
     }
-  );
-});
+  )
+})
 
 //删除文章
 router.delete("/", checkToken, function (req, res, _next) {
   // 从查询参数中获取文章 ID
-  const { id } = req.query;
+  const { id } = req.query
   // 创建 SQL 查询
   const sql = `
   DELETE FROM article 
-  WHERE id = ?`;
+  WHERE id = ?`
 
   // 执行 SQL 查询
   db.query(sql, [id], (err, result) => {
     if (err) {
-      console.error(err);
-      res.status(500).send("Server error");
+      console.error(err)
+      res.status(500).send("Server error")
     } else if (result.affectedRows === 0) {
-      res.status(404).send("Article not found");
+      res.status(404).send("Article not found")
     } else {
-      res.status(200).send("Article deleted");
+      res.status(200).send("Article deleted")
     }
-  });
-});
+  })
+})
 
 //修改文章
 router.put("/", checkToken, function (req, res, _next) {
   // 从请求体中获取文章 ID 和新的文章数据
-  const { id, ...fields } = req.body;
-  fields.topping = fields.topping ? 1 : 0;
-  fields.hidden = fields.hidden ? 1 : 0;
+  const { id, ...fields } = req.body
+  fields.topping = fields.topping ? 1 : 0
+  fields.hidden = fields.hidden ? 1 : 0
   if (Array.isArray(fields.label)) {
-    fields.label = fields.label.join(",");
+    fields.label = fields.label.join(",")
   }
 
   // 创建 SQL 查询的 SET 部分
-  const setParts = [];
-  const values = [];
+  const setParts = []
+  const values = []
   for (const [key, value] of Object.entries(fields)) {
     if (key === "createTime") {
-      setParts.push(`${key} =FROM_UNIXTIME(?)`);
-      const date = new Date(value);
-      values.push(Math.floor(date.getTime() / 1000));
+      setParts.push(`${key} =FROM_UNIXTIME(?)`)
+      const date = new Date(value)
+      values.push(Math.floor(date.getTime() / 1000))
     } else {
-      setParts.push(`${key} = ?`);
-      values.push(value);
+      setParts.push(`${key} = ?`)
+      values.push(value)
     }
   }
 
   // 如果没有接收到任何字段，返回错误
   if (setParts.length === 0) {
-    return res.status(400).send("No fields to update");
+    return res.status(400).send("No fields to update")
   }
 
   // 创建 SQL 查询
   const sql = `UPDATE article
   SET ${setParts.join(", ")}
-  WHERE id = ?`;
+  WHERE id = ?`
 
   // 添加文章 ID 到参数列表
-  values.push(id);
+  values.push(id)
 
   // 执行 SQL 查询
   db.query(sql, values, (err, result) => {
     if (err) {
+      console.error(err)
+      res.status(500).send("Server error")
+    } else if (result.affectedRows === 0) {
+      res.status(404).send("Article not found")
+    } else {
+      res.status(200).send("Article updated")
+    }
+  })
+})
+
+router.post("/upload", checkToken, function (req, res, _next) {
+  // 从请求体中获取文章 ID 和新的文章数据
+  const { title, content } = req.body
+
+  fs.writeFile(`./public/article/${title}.md`, content, (err) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send("Server error")
+    } else {
+      res.status(200).send("Article updated")
+    }
+  })
+})
+
+router.get("/articles", function (req, res, _next) {
+  const articlePath = path.join(process.cwd(), './public/article');
+
+  fs.readdir(articlePath, (err, files) => {
+    if (err) {
       console.error(err);
       res.status(500).send("Server error");
-    } else if (result.affectedRows === 0) {
-      res.status(404).send("Article not found");
     } else {
-      res.status(200).send("Article updated");
+      const fileNamesWithoutExt = files.map(file => path.parse(file).name);
+      res.status(200).send(fileNamesWithoutExt);
     }
   });
 });
 
-module.exports = router;
+module.exports = router
