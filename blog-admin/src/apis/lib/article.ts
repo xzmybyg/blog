daa9135649e25378f3f13ca2fe0d61b135b794fb
心中没有白月光 <1277215827@qq.com>
2024-05-09 14:06:05 +0800

    新增文章上传和获取已有文章列表api

--- blog-admin/src/apis/lib/article.ts
+++ blog-admin/src/apis/lib/article.ts
@@ -1,12 +1,11 @@
-
 // 获取文章列表
-export function getPageArticleList({ page = 1, pageSize = 5 }={}) {
+export function getPageArticleList({ page = 1, pageSize = 5 } = {}) {
   return axiosInstance.get("/article/", {
     params: {
       page,
       pageSize,
     },
-  });
+  })
 }
 
 export function getAllArticleList() {
@@ -14,11 +13,11 @@ export function getAllArticleList() {
     params: {
       allList: true,
     },
-  });
+  })
 }
 
 export function addArticle(data) {
-  return axiosInstance.post("/article", data);
+  return axiosInstance.post("/article", data)
 }
 
 export function deleteArticle(id) {
@@ -26,10 +25,17 @@ export function deleteArticle(id) {
     params: {
       id,
     },
-  });
+  })
 }
 
 export function updateArticle(data) {
-  return axiosInstance.put("/article", data);
+  return axiosInstance.put("/article", data)
 }
 
+export function uploadArticleFile(data) {
+  return axiosInstance.post("/article/upload", data)
+}
+
+export function getArticleFiles() {
+  return axiosInstance.get("/article/articles")
+}
