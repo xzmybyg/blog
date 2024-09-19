649e6e67e2a25dbbe0d84270fa1083e9db1b310c
心中没有白月光 <1277215827@qq.com>
2024-09-19 20:38:39 +0800

    启动前检查数据库连接

--- blog-server/app.js
+++ blog-server/app.js
@@ -3,6 +3,7 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const db = require('./utils/mysqlUtils')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -17,57 +18,70 @@ var qiniuRouter = require('./routes/qiniuSave')
 
 var app = express()
 
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
-  )
-  res.header('X-Powered-By', ' 3.2.1')
-  // res.header("Content-Type", "application/json;charset=utf-8")
-  next()
+db.connect(function (err) {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack)
+    return
+  }
+  console.log('Connected to MySQL as id ' + db.threadId)
+
+  // 启动 Express 服务
+  startServer()
 })
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+function startServer() {
+  app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
+    )
+    res.header('X-Powered-By', ' 3.2.1')
+    // res.header("Content-Type", "application/json;charset=utf-8")
+    next()
+  })
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'jade')
 
-app.use('/api/users', usersRouter)
-app.use('/api/article', articleRouter)
-app.use('/api/topic', topicRouter)
-app.use('/api/label', labelRouter)
-app.use('/api/link', linkRouter)
-app.use('/api/comment', commentRouter)
-app.use('/api/message', messageRouter)
-app.use('/api/reply', replyRouter)
+  app.use(logger('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cookieParser())
+  app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/api/qiniu', qiniuRouter)
+  app.use('/api/users', usersRouter)
+  app.use('/api/article', articleRouter)
+  app.use('/api/topic', topicRouter)
+  app.use('/api/label', labelRouter)
+  app.use('/api/link', linkRouter)
+  app.use('/api/comment', commentRouter)
+  app.use('/api/message', messageRouter)
+  app.use('/api/reply', replyRouter)
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public/blog/index.html'))
-})
+  app.use('/api/qiniu', qiniuRouter)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/blog/index.html'))
+  })
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404))
+  })
 
-  // render the error page
-  res.status(err.status || 500)
-  res.send('Server error')
-})
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.send('Server error')
+  })
+}
 
 module.exports = app
